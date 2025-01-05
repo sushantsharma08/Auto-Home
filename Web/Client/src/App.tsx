082067650262c99bc5f97d4506e7f82f39104fae
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2025-01-06 03:28:00 +0530

    ui updates

--- Web/Client/src/App.tsx
+++ Web/Client/src/App.tsx
@@ -16,28 +16,35 @@ function App() {
   const [isAuthenticated, setisAuthenticated] = useState(true);
 
   // setInterval(() => {
-    useEffect(() => {
-      let loc = Boolean(localStorage.getItem("isAuthenticated"));
-      setisAuthenticated(loc);
-      console.log(loc);
-    }, [localStorage.getItem("isAuthenticated")])
-    
+  useEffect(() => {
+    let loc = Boolean(localStorage.getItem("isAuthenticated"));
+    setisAuthenticated(loc);
+    console.log(loc);
+  }, [localStorage.getItem("isAuthenticated")])
 
-    
+
+
   // }, [1000]);
 
-console.log(isAuthenticated);
+  console.log(isAuthenticated);
 
   return (
     <div className="app">
+      <div className="hive-text flicker-out-1 ">
+        <div className="dashboard-welcome-message slide-top " >
+          Welcome to
+          <span style={{ color: "#36827F", fontSize: "65px", fontFamily: "fantasy", fontWeight: "bolder" }}> H.I.V.E</span>
+        </div>
+      </div>
       <QueryClientProvider client={queryClient}>
         <div>
           <Navbar />
         </div>
+
         <section className="mainSection">
           <Routes>
             <Route path="/" element={!isAuthenticated ? <Navigate to="/Login" /> : <Dashboard />} />
-            <Route path="/devices" element={!isAuthenticated ? <Navigate to="/Login" /> :<Relay />} />
+            <Route path="/devices" element={!isAuthenticated ? <Navigate to="/Login" /> : <Relay />} />
             <Route path="/Login/*" element={<Login />} />
             <Route path="/Register/*" element={<Registration />} />
           </Routes>
--- Web/Client/src/pages/Dashboard.tsx
+++ Web/Client/src/pages/Dashboard.tsx
@@ -2,10 +2,20 @@ const Dashboard = () => {
   return (
     <div style={{ color: "grey" }}>
       <section className="dashboard-welcome">
-        <div className="dashboard-welcome-message" >  Welcome to 
-          <span style={{ color: "#36827F",fontSize:"60px",fontFamily:"fantasy" }}> H.I.V.E</span>,
-          <br /> Home Intelligent Virtual Ecosystem</div>
 
+      <span className="hive-dashboard glow" style={{ color: "#36827F", fontSize: "65px", fontFamily: "fantasy", fontWeight: "bolder" }}> H.I.V.E</span>
+
+        <div className="dashboard-welcome-message" >
+          <br /> Home Intelligent Virtual Ecosystem
+        </div>
+        <span className="dashboard-catchphrase">"Where Comfort Meets Control."</span>
+
+        {/* <span>"Efficiency You Can Feel, Savings You’ll See."</span>
+        <span>"Smart Living Without Breaking the Bank."</span>
+        <span>"Automation That Fits Your Budget."</span>
+        <span>"Intelligence That’s Easy on Your Wallet."</span>
+        <span>"Spend Less, Automate More."</span> */}
+        <span></span>
       </section>
     </div>
   )
