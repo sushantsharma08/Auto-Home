f6f812845d98adf6d8d51c8d6e83a79de77a54fb
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2025-01-07 01:16:40 +0530

    ui fixed

--- Web/Client/src/pages/Dashboard.tsx
+++ Web/Client/src/pages/Dashboard.tsx
@@ -1,23 +1,44 @@
+
 const Dashboard = () => {
+
+
   return (
-    <div style={{ color: "grey" }}>
-      <section className="dashboard-welcome">
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
 
-      <span className="hive-dashboard glow" style={{ color: "#36827F", fontSize: "65px", fontFamily: "fantasy", fontWeight: "bolder" }}> H.I.V.E</span>
 
-        <div className="dashboard-welcome-message" >
-          <br /> Home Intelligent Virtual Ecosystem
-        </div>
-        <span className="dashboard-catchphrase">"Where Comfort Meets Control."</span>
+      <div style={{ color: "grey", zIndex: "1" }}>
 
-        {/* <span>"Efficiency You Can Feel, Savings You’ll See."</span>
+        <section className="dashboard-welcome">
+
+          <span className="hive-dashboard glow" style={{ color: "#36827F", fontSize: "65px", fontFamily: "fantasy", fontWeight: "bolder" }}> H.I.V.E</span>
+
+          <div className="dashboard-welcome-message" >
+            <br /> Home Intelligent Virtual Ecosystem
+          </div>
+          <span className="dashboard-catchphrase">"Where Comfort Meets Control."</span>
+
+          {/* <span>"Efficiency You Can Feel, Savings You’ll See."</span>
         <span>"Smart Living Without Breaking the Bank."</span>
         <span>"Automation That Fits Your Budget."</span>
         <span>"Intelligence That’s Easy on Your Wallet."</span>
         <span>"Spend Less, Automate More."</span> */}
-        <span></span>
-      </section>
+
+
+
+          <div className="relaystat">
+          
+
+          </div>
+
+        </section>
+      </div>
+
     </div>
+
   )
 }
 
